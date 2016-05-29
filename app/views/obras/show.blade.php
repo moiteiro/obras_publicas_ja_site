f0a6ad80de1c0758f848ce7d0d41ce88252a1436
Moiteiro <bruno.moiteiro@gmail.com>
2016-05-29 16:16:33 -0300

    Implementando Disqus.

--- app/views/obras/show.blade.php
+++ app/views/obras/show.blade.php
@@ -9,7 +9,24 @@
                 <h2>{{$obra->nome}}</h2>
             </div>
         </div>
+        <div id="disqus_thread"></div>
     </div>
-    {{$obra}}
 </section>
+<script>
+
+var disqus_config = function () {
+this.page.url = '<?php echo url() . "/obras"; ?>';
+this.page.identifier = '<?php echo $obra->id; ?>';
+};
+
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+
+s.src = '//obraspblicasj.disqus.com/embed.js';
+
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
 @stop
