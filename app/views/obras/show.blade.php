bc1c8a2727c24c0e1f81a84ccbdb5e14412b081f
Moiteiro <bruno.moiteiro@gmail.com>
2016-05-31 13:22:26 -0300

    Primeiro rascunho para super a Facebook e Twitter.

--- app/views/obras/show.blade.php
+++ app/views/obras/show.blade.php
@@ -1,5 +1,13 @@
 @extends('layouts.application')
 
+@section('facebook-opengraph')
+    <meta property="fb:app_id"          content="278225112566275" /> 
+    <meta property="og:type"            content="article" /> 
+    <meta property="og:url"             content="{{Request::url()}}" /> 
+    <meta property="og:title"           content="{{$obra->nome}}" /> 
+@stop
+
+
 @section('content')
 <section>
     <div class="container">
@@ -34,6 +42,12 @@
 
             </div>
         </div>
+        
+        <div class='project-social-links'>
+            <div class="fb-like" data-href="{{Request::url()}}" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+
+            <a href="https://twitter.com/share" class="twitter-share-button" data-lang="pt-br"></a>
+        </div>
 
         <!-- Conserta isso ai, Caju!!! -->
         <br><br><br><br><br>
@@ -45,10 +59,37 @@
         </div>        
     </div>
 </section>
-<script>
 
+<!-- Facebook -->
+<script>
+    (function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=278225112566275&version=v2.0";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
+
+<!-- Twitter -->
+
+<script>
+    !function(d,s,id){
+        var js,fjs=d.getElementsByTagName(s)[0];
+        p=/^http:/.test(d.location)?'http':'https';
+        if(!d.getElementById(id)){
+            js=d.createElement(s);
+            js.id=id;
+            js.src=p+'://platform.twitter.com/widgets.js';
+            fjs.parentNode.insertBefore(js,fjs);
+        }
+    }(document, 'script', 'twitter-wjs');
+</script>
+
+<!-- Disqus -->
+<script>
 var disqus_config = function () {
-this.page.url = '<?php echo url() . "/obras"; ?>';
+this.page.url = '<?php echo url() . "/obras/". $obra->id; ?>';
 this.page.identifier = '<?php echo $obra->id; ?>';
 };
 
@@ -61,5 +102,4 @@ s.setAttribute('data-timestamp', +new Date());
 (d.head || d.body).appendChild(s);
 })();
 </script>
-<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript" rel="nofollow">comments powered by Disqus.</a></noscript>
 @stop
